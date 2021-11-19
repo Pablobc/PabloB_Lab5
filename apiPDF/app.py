3d3cf775ee7e4b782e631b9151c46f0e5fc76258
Pablobc <pablobecerra120@gmail.com>
2021-11-19 01:17:22 -0300

    frontend funcionando

--- apiPDF/app.py
+++ apiPDF/app.py
@@ -1,8 +1,11 @@
 # app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 mydb = mysql.connector.connect(
   host="localhost",
---
+++ client/src/components/pdfs.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Targets</h1>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Contraseña</th>
+              <th scope="col">IP</th>
+              <th scope="col">Sistema Operativo</th>
+              <th scope="col">Version</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(dato, index) in datos" :key="index">
+              <td>{{ dato.id }}</td>
+              <td>{{ dato.contrasenia }}</td>
+              <td>{{ dato.dip }}</td>
+              <td>{{ dato.so }}</td>
+              <td>{{ dato.version }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PDFS',
+  data() {
+    return {
+      datos: [],
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/api/grida';
+      axios.get(path)
+        .then((res) => {
+          this.datos = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
