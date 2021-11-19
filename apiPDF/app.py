503cd55dab759edac74b5d0eba392c6b6e1667cc
Pablobc <pablobecerra120@gmail.com>
2021-11-18 23:26:00 -0300

    post funcionando

--- apiPDF/app.py
+++ apiPDF/app.py
@@ -10,13 +10,14 @@ mydb = mysql.connector.connect(
   password="ITKBWiCFedvha3nv9wzx",
   database="pdfdatos"
 )
-mycursor = mydb.cursor()
+
 
 print(mydb)
 
-@app.route("/grida")
+@app.route("/grida", methods=["GET","POST"])
 
 def get_data():
+    mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM datos")
     myresult = mycursor.fetchall()
     payload = []
@@ -27,4 +28,23 @@ def get_data():
         content = {}
     print(payload)
     return jsonify(payload)
+
+@app.route('/api/load', methods=["GET","POST"])
+def post_datos():
+    json_data = request.get_data(as_text=True).strip().split("&")
+    print(json_data)
+    dip = json_data[0].split("=")[1]
+    print(dip)
+    psw= json_data[1].split("=")[1]
+    print(psw)
+    so = json_data[2].split("=")[1]
+    print(so)
+    ver = json_data[3].split("=")[1]
+    print(ver)
+    mycursor = mydb.cursor()
+    mycursor.execute("INSERT INTO datos(contraseña, dip, so, version) VALUES (%s,%s,%s,%s);"
+    , (psw,dip,so,ver))
+    mydb.commit()
+    mycursor.close()
+    return '200 Ingreso exitoso'
 
