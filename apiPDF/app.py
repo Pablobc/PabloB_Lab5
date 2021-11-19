9d533a6901c0a3e879de05161ab64d06bd5b933b
Pablobc <pablobecerra120@gmail.com>
2021-11-18 22:13:40 -0300

    get funcionando

--- apiPDF/app.py
+++ apiPDF/app.py
@@ -7,7 +7,24 @@ app = Flask(__name__)
 mydb = mysql.connector.connect(
   host="localhost",
   user="pdfhacker",
-  password="ITKBWiCFedvha3nv9wzx"
+  password="ITKBWiCFedvha3nv9wzx",
+  database="pdfdatos"
 )
+mycursor = mydb.cursor()
 
 print(mydb)
+
+@app.route("/grida")
+
+def get_data():
+    mycursor.execute("SELECT * FROM datos")
+    myresult = mycursor.fetchall()
+    payload = []
+    content = {}
+    for result in myresult:
+        content = {'id':result[4],'contrasenia': result[0], 'dip': result[1], 'so': result[2], 'version': result[3]}
+        payload.append(content)
+        content = {}
+    print(payload)
+    return jsonify(payload)
+
